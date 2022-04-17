b273bc562967f88429de5e72d6eda24cc8e35e51
padmansh <pudmaansh@gmail.com>
2022-04-17 18:13:14 +0530

    add/ reset functionality

--- src/Components/Tile.js
+++ src/Components/Tile.js
@@ -118,8 +118,29 @@ const Tile = ({ r, c }) => {
     } else return false;
   };
 
+  const restart = ()=>{
+    setSelected({});
+    setP0BoxCount(0);
+    setP1BoxCount(0);
+    setLatestP0({});
+    setLatestP1({});
+    setP0Moves({});
+    setP1Moves({});
+    setPlayer(0);
+    setBoard(tiles);
+  }
+
   return (
     <div className="flex-flex-col">
+
+
+    <div className='py-2 mb-12 rounded-md cursor-pointer flex flex-row justify-center hover:bg-gray-100' onClick={restart}>
+      <div className='mr-4'>
+    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
+</svg></div>
+<div>Restart</div></div>
+
       {board?.map((row, i) => (
         <div className="flex" key={i}>
           {row?.map((block, j) => {
